e08987f82163eec977f18b79420bcd4db42a2077
Eugene21899 <145433623+Eugene21899@users.noreply.github.com>
2023-09-19 10:41:11 +0200

    Add files via upload

---
+++ database.php
@@ -0,0 +1,51 @@
+// Eugen Schmidt XX00-PR3-SU1 1129751 19.09.2023
+
+<?php
+
+// define Product Array
+$products = [
+    [
+        'id' => '213jido12',
+        'name' => 'USB-C-Stick 128GB',
+        'description' => 'Samsung USB-C-Stick 128GB',
+        'price' => 29.99,
+        'category' => 'Speichermedien'
+    ],
+    [
+        'id' => '12ocdnkl5',
+        'name' => 'USB-Festplatte 1TB',
+        'description' => 'Toshiba USB-Festplatte 1TB',
+        'price' => 99.99,
+        'category' => 'Speichermedien'
+    ],
+    [
+        'id' => '123mk89d',
+        'name' => 'Fernseher OLED 55 Zoll',
+        'description' => 'Philips OLED 55 Zoll',
+        'price' => 599.98,
+        'category' => 'Fernseher'
+    ],
+    [
+        'id' => '89dsahui029',
+        'name' => 'TFT-Display 55 Zoll',
+        'description' => 'HP TFT-Display 55 Zoll',
+        'price' => 349.00,
+        'category' => 'Monitore'
+    ],
+    [
+        'id' => '83bdsan00',
+        'name' => 'Digitalkamera 50 Megapixel',
+        'description' => 'Samsung Digitalkamera 50 Megapixel',
+        'price' => 129.49,
+        'category' => 'Kameras'
+    ],
+    [
+        'id' => '034ndjakl123',
+        'name' => 'Digitalkamera 45 Megapixel',
+        'description' => 'Sony Digitalkamera 45 Megapixel',
+        'price' => 109.49,
+        'category' => 'Kameras'
+    ]
+];
+
+?>
